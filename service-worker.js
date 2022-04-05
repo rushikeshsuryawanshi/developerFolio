c4af493d928aeb2b79458fb8d4883807ae3e3368
rushikeshsuryawanshi <suryawanshirushikesh90@gmail.com>
2022-04-05 23:02:12 +0000

    Deploying to gh-pages from  @ e9e13075dc26520ca4370990a8cbb1557598751a 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/developerFolio/precache-manifest.3454e515d17556289db9ae52a7b28741.js"
+  "/www.ihrishi.tech/precache-manifest.f54b1221f416be398cfe2d3c84787419.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/developerFolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/www.ihrishi.tech/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
